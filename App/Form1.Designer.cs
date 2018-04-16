7a08fae411ce2b7735a91bd82d27c3709af21816
quocluc <quocluc97@gmail.com>
2018-04-16 22:33:36 +0700

    Add project files.

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNhap = new System.Windows.Forms.TextBox();
+            this.txtKetQua = new System.Windows.Forms.TextBox();
+            this.btnNhap = new System.Windows.Forms.Button();
+            this.dataGrid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNhap
+            // 
+            this.txtNhap.Location = new System.Drawing.Point(12, 29);
+            this.txtNhap.Name = "txtNhap";
+            this.txtNhap.Size = new System.Drawing.Size(560, 20);
+            this.txtNhap.TabIndex = 0;
+            // 
+            // txtKetQua
+            // 
+            this.txtKetQua.Location = new System.Drawing.Point(12, 345);
+            this.txtKetQua.Name = "txtKetQua";
+            this.txtKetQua.Size = new System.Drawing.Size(576, 20);
+            this.txtKetQua.TabIndex = 1;
+            // 
+            // btnNhap
+            // 
+            this.btnNhap.AutoSize = true;
+            this.btnNhap.Location = new System.Drawing.Point(256, 371);
+            this.btnNhap.Name = "btnNhap";
+            this.btnNhap.Size = new System.Drawing.Size(75, 23);
+            this.btnNhap.TabIndex = 2;
+            this.btnNhap.Text = "OK";
+            this.btnNhap.UseVisualStyleBackColor = true;
+            this.btnNhap.Click += new System.EventHandler(this.btnNhap_Click);
+            // 
+            // dataGrid
+            // 
+            this.dataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGrid.Location = new System.Drawing.Point(12, 55);
+            this.dataGrid.Name = "dataGrid";
+            this.dataGrid.Size = new System.Drawing.Size(560, 284);
+            this.dataGrid.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 406);
+            this.Controls.Add(this.dataGrid);
+            this.Controls.Add(this.btnNhap);
+            this.Controls.Add(this.txtKetQua);
+            this.Controls.Add(this.txtNhap);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGrid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNhap;
+        private System.Windows.Forms.TextBox txtKetQua;
+        private System.Windows.Forms.Button btnNhap;
+        private System.Windows.Forms.DataGridView dataGrid;
+    }
+}
+
---
+++ App/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNhap_Click(object sender, EventArgs e)
+        {
+            string nhap = txtNhap.Text;
+            string[] arrStringNhap = nhap.Split(':');
+            string arr = arrStringNhap[1];
+            int n = arr.Length;
+            int nx = n / 5;
+            string[] arrNx = new string[nx];
+            for (int i = 0; i<nx-5; i++)
+            {
+                arrNx[i] = arr.Substring(i*5,i*5+5);
+            }
+            for (int i = 0; i < nx; i++)
+            {
+                dataGrid.Columns.Add(new DataGridViewColumn());
+            }
+            foreach (string sell in arrNx)
+            {
+                
+            }
+            
+        }
+        private static int getNum(string str)
+        {
+            int num = 0;
+            foreach (char c in str)
+            {
+                if (Char.IsNumber(c))
+                {
+                    return (int) c;
+                }
+            }
+
+            return num;
+        }
+    }
+}
+
+//S:R1(A)W1(A)R2(A)W2(A)R1(B)W1(B)R2(B)W2(B)	
